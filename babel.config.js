581c01c4c52700326a6ba9ee55e90dc7b3bc85f0
wanghairong-i <3189578410@qq.com>
2019-05-12 18:41:09 +0800

    add transform-remove-console

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  "env": {
+    "production": {
+      "plugins": ["transform-remove-console"]
+    },
+  },
   "plugins": [
     ["import", { libraryName: "@ant-design/react-native" }] // 与 Web 平台的区别是不需要设置 style
   ]
